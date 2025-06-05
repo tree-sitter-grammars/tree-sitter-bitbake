3f0070ee0fe697daec7dac7c5227480f5b3993c1
ObserverOfTime <chronobserver@disroot.org>
2025-06-05 20:05:20 +0300

    feat!: update bindings

--- Package.swift
+++ Package.swift
@@ -1,37 +1,41 @@
 // swift-tools-version:5.3
+
+import Foundation
 import PackageDescription
+
+var sources = ["src/parser.c"]
+if FileManager.default.fileExists(atPath: "src/scanner.c") {
+    sources.append("src/scanner.c")
+}
 
 let package = Package(
     name: "TreeSitterBitBake",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterBitBake", targets: ["TreeSitterBitBake"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
+    ],
     targets: [
-        .target(name: "TreeSitterBitBake",
-                path: ".",
-                exclude: [
-                    "binding.gyp",
-                    "bindings",
-                    "Cargo.toml",
-                    "examples",
-                    "grammar.js",
-                    "LICENSE",
-                    "Makefile",
-                    "package.json",
-                    "README.md",
-                    "src/grammar.json",
-                    "src/node-types.json",
-                ],
-                sources: [
-                    "src/parser.c",
-                    "src/scanner.c",
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
-    ]
+        .target(
+            name: "TreeSitterBitBake",
+            dependencies: [],
+            path: ".",
+            sources: sources,
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterBitBakeTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterBitBake",
+            ],
+            path: "bindings/swift/TreeSitterBitBakeTests"
+        )
+    ],
+    cLanguageStandard: .c11
 )
---
+++ bindings/python/tree_sitter_bitbake/__init__.pyi
@@ -0,0 +1,7 @@
+from typing import Final
+
+HIGHLIGHTS_QUERY: Final[str]
+INJECTIONS_QUERY: Final[str]
+LOCALS_QUERY: Final[str]
+
+def language() -> object: ...
